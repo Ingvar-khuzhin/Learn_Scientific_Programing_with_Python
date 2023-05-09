c1800dea9ad02bf03d7351afb4c3e2fac8116c90
Ingvar-khuzhin <ingvar.khuzhin@mail.ru>
2023-05-09 23:21:54 +0300

    Question 2.2.8.

--- Chapter 2/exercise_2.2.py
+++ Chapter 2/exercise_2.2.py
@@ -1,3 +1,5 @@
+import math
+
 print("Question 2.2.4")
 i = 1j
 print(i**i)
@@ -12,3 +14,11 @@ print("Question 2.2.7")
 print(f"a = 2, b = 6, top edge = {d6_top_edge(2, 6)}")
 print(f"a = 3, b = 5, top edge = {d6_top_edge(3, 5)}")
 print()
+
+
+def half_paper(t: float, d: float) -> int:
+    return int(math.ceil(math.log2(d / t)))
+
+
+print("Question 2.2.8")
+print(f"t = 0.1, d = 384 400, fold in half {half_paper(0.1, 384_400)} times.")
