04e46f6531693a6b5e3c2b081d9ee982f5a47ef9
ingvar <ingvar.khuzhin@mail.ru>
2023-05-10 14:58:25 +0300

    Add file with problems to chapter 2.2

---
+++ Chapter_2/problems_2.2.py
@@ -0,0 +1,9 @@
+import math
+
+print("Problem 2.2.2")
+
+print(f"sin(2017 * 2 ** (1/5)) = {math.sin(2017 * 2 ** (1 / 5))}")
+print(f"(pi + 20) ** i = {(math.pi + 20) ** 1j}")
+
+print(f"e ** 100 = {math.e ** 100}")
+print(f"1000! = {math.factorial(1000)}")
